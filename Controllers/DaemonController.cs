950c9c32ace54dd30c1874e716887ddd2a4ba47a
MrazFilip <filda.mraz@volny.cz>
2022-04-24 21:21:18 +0200

    Update 24.4.2022

--- Controllers/DaemonController.cs
+++ Controllers/DaemonController.cs
@@ -36,5 +36,11 @@ namespace RaFilDaAPI.Controllers
 
             return daemonList;
         }
+        
+        [HttpGet("GetConmConfByCompID&ConfID")]
+        public IQueryable<Computer> GetComputers_ByConfigID(int confId, int compId)
+        {
+            return myContext.Computers.FromSqlRaw("select c.ID from CompConfs c where c.ConfigID = {0} AND c.CompID = {1}", confId, compId);
+        }
     }
 }
