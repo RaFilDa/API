1fa8e00efe593e936cf9c13fdba30b061523bfaf
radecek04 <radek.piterak@gmail.com>
2022-06-05 21:54:47 +0200

    Fixed unlimited tokens

--- Controllers/SessionsController.cs
+++ Controllers/SessionsController.cs
@@ -56,6 +56,7 @@ namespace RaFilDaAPI.Controllers
                 token = JwtBuilder.Create()
                     .WithAlgorithm(new HMACSHA256Algorithm()) // symmetric
                     .WithSecret(AuthenticationService.SECRET)
+                    .AddClaim("exp", DateTimeOffset.MaxValue.ToUnixTimeSeconds())
                     .AddClaim("role", "daemon")
                     .Encode();
             else
